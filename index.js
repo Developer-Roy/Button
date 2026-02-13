456d957d56ad328fc336aa5ff20dc5c60aa28f52
Developer-Roy <163244767+Developer-Roy@users.noreply.github.com>
2026-02-13 20:24:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// const buttonContainers = document.querySelectorAll("button-container");
+
+const button2 = document.querySelector(".button2");
+
+button2.addEventListener("click", () => {
+    button2.classList.toggle("active");
+})
